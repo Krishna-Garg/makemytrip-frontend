dc27e4c3ff0a0f6e0a149155a823c44dfb0a487b
Krishna-Garg <krishna.garg.dlp@gmail.com>
2026-09-14 14:36:09 +0530

    Navbar and TopBar collapse

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
     dispatch(clearUser());
   };
   return (
-    <header className=" backdrop-blur-md py-4 sticky top-0 z-50">
+    <header className=" backdrop-blur-md py-4 sticky top-0 ">
       <div className="container mx-auto px-4 flex items-center justify-between">
         <div className="flex items-center space-x-2 text-white cursor-pointer" onClick={() => router.push("/")}>
           <Plane className="w-8 h-8 text-red-500" />
